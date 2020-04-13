ba2c8a318f7915e38c01d7ebc677fa82317769d2
martyminion <martkimwaweru@gmail.com>
2020-04-13 19:17:49 +0300

    fixes deployment issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  token:"bc991e60bc13330311de9154bbb2a790a58",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  token:"bc991e60bc13330311de9154bbb2a790a58"
+  token:"bc991e60bc13330311de9154bbb2a790a58",
 };
 
 /*
